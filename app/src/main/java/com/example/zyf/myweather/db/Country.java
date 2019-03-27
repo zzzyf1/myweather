286f501cc410a7f452746909599b4fa8a6b05580
zzzyf1 <3032568752@qq.com>
2019-03-27 15:16:58 +0800

    create databases

---
+++ app/src/main/java/com/example/zyf/myweather/db/Country.java
@@ -0,0 +1,42 @@
+package com.example.zyf.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+    private int id;
+    private int cityId;
+    private String countryName;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
